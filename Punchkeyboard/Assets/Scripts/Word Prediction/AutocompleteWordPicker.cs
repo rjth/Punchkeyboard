6a083d9af8971afd2a3326c51904dbdc1efc5792
Jonathan Ravasz <contact@jonathanravasz.com>
2017-02-21 18:51:57 +0100

    removed commented lines

--- Punchkeyboard/Assets/Scripts/Word Prediction/AutocompleteWordPicker.cs
+++ Punchkeyboard/Assets/Scripts/Word Prediction/AutocompleteWordPicker.cs
@@ -11,10 +11,6 @@ public class AutocompleteWordPicker : MonoBehaviour
 	public InputField TextField;
 	public NGramGenerator WordPredictor;
 
-	void Start()
-	{
-		
-	}
 	public void ReplaceWord(string correctWord)
 	{
 		List<string> inputText = new List<string>();
@@ -39,37 +35,7 @@ public class AutocompleteWordPicker : MonoBehaviour
 
 		WordPredictor.PredictNextWords(correctWord);
 	}
-	/*
-	public void ReplaceWord(string correctWord)
-	{
-		List<string> inputText = new List<string>();
-		StringBuilder builder = new StringBuilder();
-		string input = TextField.text;
-		input = ReverseString (input);
-		input = input.Substring (input.IndexOf (' ') + 1);
-		input = ReverseString (input);
-		Debug.Log (input);
 
-		string[] parts = input.Split(' ');
-		parts = parts.Take(parts.Length - 1).ToArray();
-
-		for(int i = 0; i < parts.Length; i++)
-		{
-			inputText.Add(parts[i]);
-		}
-
-		inputText.Add(correctWord);
-
-		foreach (string w in inputText)
-		{
-			builder.Append(w).Append(" ");
-		}
-		TextField.text = builder.ToString();
-		TextField.ActivateInputField();
-
-		WordPredictor.PredictNextWords(correctWord);
-	}
-	*/
 	public static string ReverseString(string s)
 	{
 		char[] charArray = s.ToCharArray ();
